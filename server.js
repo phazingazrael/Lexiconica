7f55a8dc356b85fe2d9633e9387012c9c97b25a3
Cameren Dessert <phazedverse@gmail.com>
2022-04-15 01:19:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,93 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World, Welcome to a Electron app");
+const Datastore = require("happn-nedb");
+const srd = new Datastore({ filename: "./public/data/srd", autoload: true });
+const usrd = new Datastore({ filename: "./public/data/usrd", autoload: false });
+const campaign = new Datastore({
+  filename: "./public/data/campaign",
+  autoload: true,
+});
+// You can issue commands right away
+
+const srdData = require("./src/data/usrd/");
+
+//default post/get routes.
+
+app.get("/", (req, res) => {
+  return res.send("Received a GET HTTP method");
+  //return res.send(srd);
 });
 
-app.use("/", router);
+app.post("/", (req, res) => {
+  //const srd = new Datastore({ filename: './public/data/srd', autoload: true });
+  var doc = {
+    actions: {},
+    adventure: {},
+    adventures: {},
+    backgrounds: {},
+    backgroundsFluff: {},
+    bestiary: {},
+    book: {},
+    books: {},
+    charOpts: {},
+    charOptsFluff: {},
+    classes: {},
+    conDisFluff: {},
+    conditionsDisease: {},
+    cultsBoons: {},
+    deities: {},
+    encounters: {},
+    feats: {},
+    items: {},
+    itemsBase: {},
+    itemsFluff: {},
+    lang: {},
+    langFluff: {},
+    life: {},
+    loot: {},
+    magicVariants: {},
+    masterCR: {},
+    monsterfeatures: {},
+    monstertraits: {},
+    names: {},
+    objects: {},
+    optionalfeatures: {},
+    psionics: {},
+    races: {},
+    racesFluff: {},
+    recipeFluff: {},
+    recipes: {},
+    rewards: {},
+    spells: {},
+    tables: {},
+    trapsHazards: {},
+    variantrules: {},
+  };
+  usrd.insert(doc);
+
+  return res.send(srdData);
+});
+
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
+});
+
+//srd api routes
+
+app.get("/srd", (req, res) => {
+  //return res.send('Received a GET HTTP method');
+  return res.send(srd);
+});
+
+app.get("/usrd", (req, res) => {
+  //return res.send('Received a GET HTTP method');
+  return res.send(usrd);
+});
 
 app.listen(process.env.port || 2048);
 
