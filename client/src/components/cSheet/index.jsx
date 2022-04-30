05efe4ebbfd9d61abd163a3f4d469da360e80c0f
Cameren Dessert <phazedverse@gmail.com>
2022-04-29 22:46:46 -0600

    Various maintenance updates and minor improvements.
    ### Changes
    - Modified layout to be more responsive to screen sizing.
    - Removed unnecessary packages.
    - Moved Development packages to the correct section.
    - Organization of package.json file.
    - Relocated the Changelog link location and implemented package.json version display for changelog link.
    - Implemented base level api using [ExpressJS](https://expressjs.com/).

---
+++ client/src/components/cSheet/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css';
+
+function Characters() {
+  return (
+    <div>
+      <p>return data here</p>
+    </div>
+  );
+}
+
+export default Characters;
--- server.js
+++ server.js
@@ -1,94 +1,33 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
+const port = process.env.PORT || 5000;
 
-const Datastore = require("happn-nedb");
-const srd = new Datastore({ filename: "./public/data/srd", autoload: true });
-const usrd = new Datastore({ filename: "./public/data/usrd", autoload: false });
-const campaign = new Datastore({
-  filename: "./public/data/campaign",
-  autoload: true,
-});
-// You can issue commands right away
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const srdData = require("./src/data/usrd/");
-
-//default post/get routes.
-
-app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP method");
-  //return res.send(srd);
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
 });
 
-app.post("/", (req, res) => {
-  //const srd = new Datastore({ filename: './public/data/srd', autoload: true });
-  var doc = {
-    actions: {},
-    adventure: {},
-    adventures: {},
-    backgrounds: {},
-    backgroundsFluff: {},
-    bestiary: {},
-    book: {},
-    books: {},
-    charOpts: {},
-    charOptsFluff: {},
-    classes: {},
-    conDisFluff: {},
-    conditionsDisease: {},
-    cultsBoons: {},
-    deities: {},
-    encounters: {},
-    feats: {},
-    items: {},
-    itemsBase: {},
-    itemsFluff: {},
-    lang: {},
-    langFluff: {},
-    life: {},
-    loot: {},
-    magicVariants: {},
-    masterCR: {},
-    monsterfeatures: {},
-    monstertraits: {},
-    names: {},
-    objects: {},
-    optionalfeatures: {},
-    psionics: {},
-    races: {},
-    racesFluff: {},
-    recipeFluff: {},
-    recipes: {},
-    rewards: {},
-    spells: {},
-    tables: {},
-    trapsHazards: {},
-    variantrules: {},
-  };
-  usrd.insert(doc);
-
-  return res.send(srdData);
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
 });
 
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
-});
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
-//srd api routes
-
-app.get("/srd", (req, res) => {
-  //return res.send('Received a GET HTTP method');
-  return res.send(srd);
-});
-
-app.get("/usrd", (req, res) => {
-  //return res.send('Received a GET HTTP method');
-  return res.send(usrd);
-});
-
-app.listen(process.env.port || 2048);
-
-console.log("Web Server is listening at port " + (process.env.port || 2048));
+app.listen(port, () => console.log(`Listening on port ${port}`));
